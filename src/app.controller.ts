e430ec27538c491de268934cc61fd58cf2357ee6
Sakalito <wassim.houiat@gmail.com>
2022-05-12 11:18:50 +0200

    importation du projet

---
+++ src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+  @Get('blog')
+  getBlog(): string {
+    return "liste d'article";
+  }
+}
