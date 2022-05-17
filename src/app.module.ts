bc6c87f92de65d2815cf248d340c2ec5609dac2e
E-jarod <ejilane.jarod@gmail.com>
2022-05-17 14:54:09 +0200

    users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { PrismaService } from './shared/services/prisma/prisma.service';
 import { PostsModule } from './posts/posts.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [PostsModule, UsersModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
