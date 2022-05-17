85b49024af6a852a18ccab4e610ea35be4cd4b07
E-jarod <ejilane.jarod@gmail.com>
2022-05-17 15:06:46 +0200

    post service with prisma

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,60 @@
 import { Injectable } from '@nestjs/common';
+import { Post, Prisma } from '@prisma/client';
+
+import { PrismaService } from 'src/shared/services/prisma/prisma.service';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findAllPosts(params: {
+    skip?: number;
+    take?: number;
+    cursor?: Prisma.PostWhereUniqueInput;
+    where?: Prisma.PostWhereInput;
+    // orderBy?: Prisma.PostWithRelationInput;
+  }): Promise<Post[] | undefined> {
+    const { skip, take, cursor, where } = params;
+    return this.prisma.post.findMany({
+      skip,
+      take,
+      cursor,
+      where,
+      // orderBy,
+    });
+  }
+
+  async findOnePost(
+    postWhereUniqueInput: Prisma.PostWhereUniqueInput,
+  ): Promise<Post | null> {
+    return this.prisma.post.findUnique({ where: postWhereUniqueInput });
+  }
+
+  async createPost(
+    user: Prisma.PostCreateInput,
+  ): Promise<Post | undefined> {
+    return this.prisma.post.create({
+      data: user,
+    });
+  }
+
+  async updatePost(params: {
+    where: Prisma.PostWhereUniqueInput;
+    data: Prisma.PostUpdateInput;
+  }): Promise<Post> {
+    const { where, data } = params;
+
+    return this.prisma.post.update({
+      data,
+      where,
+    });
+  }
+
+  async deletePost(
+    where: Prisma.PostWhereUniqueInput,
+  ): Promise<Post | undefined> {
+    return this.prisma.post.delete({
+      where,
+    });
+  }
+}
