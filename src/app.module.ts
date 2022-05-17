f7bcd18214059e936d1e2f9855192874240e2656
E-jarod <ejilane.jarod@gmail.com>
2022-05-17 14:52:14 +0200

    post module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { PrismaService } from './shared/services/prisma/prisma.service';
 
+import { PostsModule } from './posts/posts.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 @Module({
-  imports: [],
+  imports: [PostsModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
